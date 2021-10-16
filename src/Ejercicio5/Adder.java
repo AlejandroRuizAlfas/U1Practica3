855cb217ebe1afa9ec2c3c3abcaca43bf055637c
AlejandroRuizAlfas <aruizalfas@gmail.com>
2021-10-17 01:07:13 +0200

    [MOD] Clase Adder

--- src/Ejercicio5/Adder.java
+++ src/Ejercicio5/Adder.java
@@ -1,4 +1,28 @@
 package Ejercicio5;
+import java.io.*;
+import java.util.Scanner;
 
 public class Adder {
+    public static void main(String[] args) {
+        String fichero = args[0];
+        Scanner sc=null;
+        int sumaTotal = 0;
+        try {
+            sc = new Scanner(new BufferedReader(new FileReader(fichero)));
+
+            while (sc.hasNext()){
+                if (sc.hasNextInt())
+                    sumaTotal += sc.nextInt();
+                else
+                    sc.next();
+            }
+            System.out.println(sumaTotal);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            sc.close();
+        }
+    }
 }
